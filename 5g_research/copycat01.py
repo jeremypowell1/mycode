c4dfb498af1af1988f2ae7dc8124139a63d1e86c
Jeremy Powell <jeremypowell1@gmail.com>
2019-07-08 15:12:12 -0400

    added the shutil copy lab  - JP

---
+++ 5g_research/copycat01.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+import shutil
+import os
+
+os.chdir('/home/student/mycode/')
+
+shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
+
+# The following line will create the directory if it does not exist already
+
+shutil.copytree('5g_research/', '5g_research_backup/')
