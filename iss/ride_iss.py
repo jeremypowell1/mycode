bfba23358a52d4a7de846a4585fffc21edebc927
Jeremy Powell <jeremypowell1@gmail.com>
2019-07-09 09:16:19 -0400

    Learning to track ISS via API call - JP

---
+++ iss/ride_iss.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+"""Alta3| Tracking ISS"""
+
+import urllib.request
+import json
+
+## URL
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+def main():
+    # Call the webservice
+    groundctrl = urllib.request.urlopen(MAJORTOM)
+    # put fileobject into helmet
+    helmet = groundctrl.read()
+    # Decode json to python data structure
+    helmetson = json.loads(helmet.decode('utf-8'))
+    # Display pythonic data
+    #print("\n\nConverted Python data")
+    #print(helmetson)
+
+    print('\n\nPeople in Space: ', helmetson['number'])
+    people = helmetson['people']
+    #print(people)
+
+    for x in helmetson['people']:
+        print(x.get('name') + " on the " + x.get('craft'))
+
+main()
